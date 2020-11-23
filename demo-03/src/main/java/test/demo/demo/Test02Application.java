5f34cd769e832e65667ba671d6de31f0d0ac18b9
arm253489 <63191129+arm253489@users.noreply.github.com>
2020-11-23 12:56:42 +0700

    initail

---
+++ demo-03/src/main/java/test/demo/demo/Test02Application.java
@@ -0,0 +1,12 @@
+package test.demo.demo;
+
+
+
+@SpringBootApplication
+public class Test02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test02Application.class, args);
+    }
+
+}
